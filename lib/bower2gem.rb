0e3454aee02c1dabd1903eb29152de67ed91851f
Sean Collins <sean@cllns.com>
2015-10-23 20:11:03 -0600

    Inital commit

---
+++ lib/bower2gem.rb
@@ -0,0 +1,5 @@
+require "bower2gem/version"
+
+module Bower2gem
+  # Your code goes here...
+end
---
+++ spec/bower2gem_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Bower2gem do
+  it 'has a version number' do
+    expect(Bower2gem::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
