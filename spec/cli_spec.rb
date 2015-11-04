3de1b4e121d0ac5113e263c05cf95ea03cfa9a3b
Sean Collins <sean@cllns.com>
2015-11-03 22:54:37 -0700

    Update test for File Downloader

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,7 +2,8 @@ require "spec_helper"
 require "bower2gem/cli"
 
 describe Bower2Gem::CLI do
-  it "does something useful" do
-    expect { Bower2Gem::CLI.new }.to output("dist/rome.js\n").to_stdout
+  it "downloads javascript file" do
+    Bower2Gem::CLI.new
+    expect(File).to exist("vendor/assets/rome.js")
   end
 end
