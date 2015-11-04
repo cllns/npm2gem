198c342cba7582f863c75018f00db3de368bb91a
Sean Collins <sean@cllns.com>
2015-11-04 11:01:55 -0700

    Update bower install spec to use bootstrap

--- spec/bower_install_spec.rb
+++ spec/bower_install_spec.rb
@@ -3,8 +3,8 @@ require "bower2gem/bower_install"
 
 describe Bower2Gem::BowerInstall do
   it "install package with bower" do
-    Bower2Gem::BowerInstall.new("rome")
-    expect(Dir).to exist("bower_components/rome")
-    expect(File).to exist("bower_components/rome/dist/rome.js")
+    Bower2Gem::BowerInstall.new("bootstrap")
+    expect(Dir).to exist("bower_components/bootstrap")
+    expect(File).to exist("bower_components/bootstrap/dist/js/bootstrap.js")
   end
 end
